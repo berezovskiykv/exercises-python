ee3a109dcf7984612146b4d39ac4811d060223f7
berezovskiykv <50821233+berezovskiykv@users.noreply.github.com>
2019-05-22 13:39:38 +0700

    Update index.py
    #Данный пример прекрасно отрабатывает на https://repl.it, но на https://code-basics.ru упорно выдает ошибку.
    def get_age_difference(birth_year_one, birth_year_two):
        dif_age = abs(birth_year_one - birth_year_two)
        return print('The age difference is %d' % (dif_age))
    
    #Предложенный же вами пример не возвращает разницу значений на https://repl.it, но отрабатывает на https://code-basics.ru. 
    #def get_age_difference(year_one, year_two):
    #    difference = abs(year_one - year_two)
    #    return "The age difference is {}".format(difference)    
    
    #get_age_difference(1996, 1987)

--- modules/40-define-functions/400-environment/index.py
+++ modules/40-define-functions/400-environment/index.py
@@ -1,3 +1,7 @@
-def get_age_difference(year_one, year_two):
-    difference = abs(year_one - year_two)
-    return "The age difference is {}".format(difference)
+def get_age_difference(birth_year_one, birth_year_two):
+    dif_age = abs(birth_year_one - birth_year_two)
+    return print('The age difference is %d' % (dif_age))
+
+#def get_age_difference(year_one, year_two):
+#    difference = abs(year_one - year_two)
+#    return "The age difference is {}".format(difference)  
